39a2bc68e0cdbbda522d1eafcc44331353f6096e
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-24 03:53:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ const divdes = (a,b) =>{
 const inc = n =>{
   return parseInt(n,10)+1
 }
+
+const dec = n =>{
+  return parseInt(n,10)-1
+}
+
+const makeInt =n =>{
+  return parseInt(n,10)
+}
+
+const preserveDecimal = n =>{
+  return parseFloat(n)
+}
+
