5f3bf09c562382c93608b98abb40d50101020ba8
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-24 03:51:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const add = (a,b) =>{
   return a+b
 }
 
-const subtract = (a,b) ={
+const subtract = (a,b) =>{
   return b-a
+}
+
+const multiply = (a,b) =>{
+  return a*b
+}
+
+const divdes = (a,b) =>{
+  return b/a
+}
+
+const inc = n =>{
+  return parseInt(n,10)+1
 }
